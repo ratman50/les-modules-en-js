5352c7d68ea310d7c7b605cf46a61283520b75d1
pape mactar ndiaye <ratma96@gmail.com>
2021-12-14 22:56:45 +0000

    javascript main fileé

---
+++ main.js
@@ -0,0 +1,11 @@
+//imprt in curly braces the funnction sayHi
+// import { sayHi } from "./mod1.js";
+
+//import two functions from mod2.js
+
+import {Sum,decremente} from "./mod2.js"
+
+Sum(7);
+// console.log(decremente(3))
+document.body.innerText+=decremente(3)
+// sayHi("matar");
